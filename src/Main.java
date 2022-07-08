fb7451e711cf5bd698db5b7a7d4ab9cb18f8ef33
kquach229 <kquach229>
2022-07-07 22:39:16 -0400

    git diff

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String [] args) {
+        String name = "  Kenny Quach      ";
+        String result = name.replace("e", "a");
+        System.out.println(result);
+
+        }
+    }
+
